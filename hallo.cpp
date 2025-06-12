6cf85ca139aeeb2ee55af41b1e3c624727446fb4
rifkitaruna <rifkitaruna5@example.com>
2025-06-12 12:19:39 +0800

    memasukan data 5 buah nama

--- hallo.cpp
+++ hallo.cpp
@@ -5,6 +5,13 @@ using namespace std;
 int main() {
     // Inisialisasi array string untuk 5 nama
     string nama[5];
-    
+
+    // Memasukkan 5 nama
+    nama[0] = "rifki";
+    nama[1] = "belfadilah";
+    nama[2] = "raka";
+    nama[3] = "dustyn";
+    nama[4] = "fuad";
+
     return 0;
 }
